e19dfd4aa7b5ca4fea6eaaa8db84835a64b8e748
tyoc213 <tyoc213+git@tyoc.mx>
2022-06-27 22:25:59 -0500

    multiple commits

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
 use std::io::stdin;
 use std::process::Command;
+use std::io::stdout;
+use std::io::Write;
 
-fn main() {
+
+fn repl() {
+    // use the `>` character as the prompt
+    // need to explicitly flush this to ensure it prints before `read_line`
+    print!("> ");
+    stdout().flush();
+
     let mut input = String::new();
     stdin().read_line(&mut input).unwrap();
 
     // read_line leaves a trailing newline, which rtim removes
     let command = input.trim();
 
-    Command::new(command)
+    let mut child = Command::new(command)
         .spawn()
         .unwrap();
+
+    // don't accept another command until this one completes
+    child.wait();
+}
+
+fn main() {
+    loop {
+        repl();
+    }
 }
